2ead8b97e2da170f98c4bffc1218404325d20fe1
luisochoa-beep <luisjulianochoaoliv@gmail.com>
2025-10-27 20:29:52 -0600

    Archivo P>HP para borrar un Usuario de la base de Datos

---
+++ borrar.php
@@ -0,0 +1,11 @@
+<?php
+include 'bd.php';
+
+$id = $_GET['id'];
+$conexion = conectar();
+$result = pg_query_params($conexion, "DELETE FROM usuarios WHERE id = $1", [$id]);
+pg_close($conexion);
+
+header('Location: index.php');
+exit;
+?>a
